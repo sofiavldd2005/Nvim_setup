629a22afc0ba81eb85051178f9741896c75c317e
Sofia Vazquez <sofia.vazquez.duarte@tecnico.ulisboa.pt>
2026-03-14 22:28:46 +0000

    Changes to init.lua and keymaps.lua
    In init.lua, the debug configs for my embedded rust are loaded, to use probe-rs. prolly there's a better way to load the debug configs than this way for sure.
    keymaps.lua changed, to add shortcut for the debug actions, to make it equal to vscode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,52 @@
-vim.keymap.set("n", "<leader>/", function() Snacks.terminal.toggle(nil, { win = { position = "bottom" } }) end, { desc = "Terminal (Horizontal)" })-- Keymaps are automatically loaded on the VeryLazy event
+vim.keymap.set("n", "<leader>/", function()
+  Snacks.terminal.toggle(nil, { win = { position = "bottom" } })
+end, { desc = "Terminal (Horizontal)" }) -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+-- ==========================================
+-- Debugging (DAP) Keymaps
+-- ==========================================
+
+-- F5: Start or Continue execution
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "DAP: Continue" })
+
+-- F10: Step Over (Execute current line, don't go into functions)
+vim.keymap.set("n", "<F10>", function()
+  require("dap").step_over()
+end, { desc = "DAP: Step Over" })
+
+-- F11: Step Into (Go inside the function call on the current line)
+vim.keymap.set("n", "<F11>", function()
+  require("dap").step_into()
+end, { desc = "DAP: Step Into" })
+
+-- F12: Step Out (Finish the current function and jump back out)
+vim.keymap.set("n", "<F12>", function()
+  require("dap").step_out()
+end, { desc = "DAP: Step Out" })
+
+-- F9: Toggle Breakpoint (Drop a stop-sign on the current line)
+vim.keymap.set("n", "<F9>", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "DAP: Toggle Breakpoint" })
+
+-- Optional: Open the REPL (Debug Console) to inspect variables
+vim.keymap.set("n", "<leader>dr", function()
+  require("dap").repl.toggle()
+end, { desc = "DAP: Toggle REPL" })
+
+-- Shift+F5: Terminate the session and close the UI
+vim.keymap.set("n", "<leader>dq", function()
+  -- 1. Stop the debugger
+  require("dap").terminate()
+
+  -- 2. Close the built-in REPL
+  require("dap").repl.close()
+
+  -- 3I. (Optional) If you are using the 'nvim-dap-ui' plugin, uncomment the line below to close it:
+  require("dapui").close()
+
+  print("Debug session terminated.")
+end, { desc = "DAP: Terminate & Close UI" })
